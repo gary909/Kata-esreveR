b41f1ad8760d53340ecba645557c2e938a9883a6
gary909 <garywhite61@hotmail.com>
2022-10-18 13:08:14 +0200

    code working

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
-reverse = function (array) {
-    // TODO: program me!
+// Can't use built in functions :-(
+// function reverse(array) {
+//     return array.reverse();
+// }
+
+function reverse(array) {
+    let myArr = [];
+    for (let i = 0; i < array.length; i++) {
+        myArr.unshift(array[i]);
+    }
+    return myArr;
 }
 
 console.log(reverse([1, 2, 3])); // [3,2,1]
